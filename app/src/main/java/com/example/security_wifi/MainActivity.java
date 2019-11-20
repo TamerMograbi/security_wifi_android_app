c63299062c2a8d7890bdfabc8c345eafca539a46
TamerMograbi <tomispk@gmail.com>
2019-11-20 17:57:02 -0500

    added socket communication using socket.io
    the android app connects to a socket on thes server of https://murmuring-wildwood-99815.herokuapp.com/  and then this server sends an appropriate message to whomever is connected to it
    
    in order to try it:
    open up https://murmuring-wildwood-99815.herokuapp.com/
    click on any of the wifi names in the list and see that a pattern of colors appears.
    
    for now it always shows this pattern because i send the same one for any of the selections but this can easily be changed once we know how we'll do the study

--- app/src/main/java/com/example/security_wifi/MainActivity.java
+++ app/src/main/java/com/example/security_wifi/MainActivity.java
@@ -7,8 +7,29 @@ import android.widget.Toast;
 import android.view.View;
 import android.widget.TextView;
 import android.util.Log;
+import java.net.URISyntaxException;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.*;
 
 public class MainActivity extends ListActivity {
+    private Socket mSocket;
+    {
+        try {
+            //try to visit this url on your laptop and see that clicking on a wifi updates
+            //this page
+            mSocket = IO.socket("https://murmuring-wildwood-99815.herokuapp.com/");
+        } catch (URISyntaxException e) { Log.d("myTag", "failed to connect");}
+    }
 
     static final String[] wifi_names =
             new String[]{"eduroam", "cafe", "cafe_guest", "cafe_5G"};
@@ -16,6 +37,7 @@ public class MainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSocket.connect();
 
         setListAdapter(new MobileArrayAdapter(this, wifi_names));
         TextView textView = new TextView(this);
@@ -34,6 +56,55 @@ public class MainActivity extends ListActivity {
         //get selected items
         String selectedValue = (String) getListAdapter().getItem(position-1);
         Toast.makeText(this, "connected to " + selectedValue, Toast.LENGTH_SHORT).show();
+
+        JSONObject jsonObject1 = new JSONObject();
+        JSONObject jsonObject2 = new JSONObject();
+        JSONObject jsonObject3 = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        JSONObject colorsObj = new JSONObject();
+        try {
+            jsonObject1 = new JSONObject();
+            jsonObject1.put("color","red");
+            jsonObject2 = new JSONObject();
+            jsonObject2.put("color","green");
+            jsonObject3 = new JSONObject();
+            jsonObject3.put("color","blue");
+
+            jsonArray.put(jsonObject1);
+            jsonArray.put(jsonObject2);
+            jsonArray.put(jsonObject3);
+
+            colorsObj.put("colors", jsonArray);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mSocket.emit("news",colorsObj);
+//below is code for an http request. it works but using sockets is easier
+/*        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://murmuring-wildwood-99815.herokuapp.com/";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                        Log.d("myTag", "Response is: "+ response.substring(0,404));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+                Log.d("myTag", "That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);*/
 
     }
 }
