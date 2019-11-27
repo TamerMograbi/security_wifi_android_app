db564c113d07429d576cd37819acd383df9840e3
TamerMograbi <tomispk@gmail.com>
2019-11-26 19:11:01 -0500

    added correct and wrong pattern
    phone sends correct pattern to server when clicking on right wifi.
    phone sends wrong pattern to server when clicking on attacker.
    says wifi doesn't support blinking if we click on the other wifis.

--- app/src/main/java/com/example/security_wifi/ThirdScenario.java
+++ app/src/main/java/com/example/security_wifi/ThirdScenario.java
@@ -46,6 +46,16 @@ public class ThirdScenario extends ListActivity {
 
     private Socket mSocket;
 
+    private int attacker_pos = 1;
+    private int real_wifi_pos = 3;
+
+
+    String[] correct_pattern_server = {"red","green","blue"};
+    String[] wrong_pattern_server = {"MediumVioletRed","PowderBlue","LightCoral"};
+
+    int[] correct_pattern_phone = {Color.parseColor("#FF0000"),Color.parseColor("#00FF00"),Color.parseColor("#0000FF")};
+    int[] wrong_pattern_phone = {Color.parseColor("#C71585"),Color.parseColor("#B0E0E6"),Color.parseColor("#F08080")};
+
     View blinker;
 
     long tStart;
@@ -73,14 +83,72 @@ public class ThirdScenario extends ListActivity {
     static final int[] wifi_safety =
             new int[]{0,0,0,0,0,0};
 
+
+
+    void sendBlinkRequestToServer(String color1,String color2, String color3) {
+        JSONObject jsonObject1 = new JSONObject();
+        JSONObject jsonObject2 = new JSONObject();
+        JSONObject jsonObject3 = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        JSONObject colorsObj = new JSONObject();
+        try {
+            jsonObject1 = new JSONObject();
+            jsonObject1.put("color",color1);
+            jsonObject2 = new JSONObject();
+            jsonObject2.put("color",color2);
+            jsonObject3 = new JSONObject();
+            jsonObject3.put("color",color3);
+
+            jsonArray.put(jsonObject1);
+            jsonArray.put(jsonObject2);
+            jsonArray.put(jsonObject3);
+
+            colorsObj.put("colors", jsonArray);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mSocket.emit("news",colorsObj);
+    }
+
+    void makePhoneBlink(final View blinkerView,final int color1,final int color2,final int color3) {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                blinkerView.setBackgroundColor(color1);
+                blinkerView.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        blinkerView.setBackgroundColor(color2);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                blinkerView.setBackgroundColor(color3);
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        blinkerView.setVisibility(View.INVISIBLE);
+                                    }
+                                }, 1000);
+                            }
+                        }, 1000);
+                    }
+                }, 1000);
+            }
+        }, 1000);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_scenario);
         mSocket.connect();
         tStart = System.currentTimeMillis();
-
         final View blinker = findViewById(R.id.blinker);
+
         blinker.setVisibility(View.INVISIBLE);
 
         final MobileArrayAdapter adapter = new MobileArrayAdapter(this, wifi_names, wifi_strengths, wifi_private, wifi_safety);
@@ -95,62 +163,80 @@ public class ThirdScenario extends ListActivity {
                     return true;
                 }
                 String selectedValue = (String) getListAdapter().getItem(position-1);
-                Toast.makeText(getApplicationContext(), "asking " +selectedValue + " to blink " , Toast.LENGTH_SHORT).show();
-
-                JSONObject jsonObject1 = new JSONObject();
-                JSONObject jsonObject2 = new JSONObject();
-                JSONObject jsonObject3 = new JSONObject();
-                JSONArray jsonArray = new JSONArray();
-                JSONObject colorsObj = new JSONObject();
-                try {
-                    jsonObject1 = new JSONObject();
-                    jsonObject1.put("color","red");
-                    jsonObject2 = new JSONObject();
-                    jsonObject2.put("color","green");
-                    jsonObject3 = new JSONObject();
-                    jsonObject3.put("color","blue");
-
-                    jsonArray.put(jsonObject1);
-                    jsonArray.put(jsonObject2);
-                    jsonArray.put(jsonObject3);
-
-                    colorsObj.put("colors", jsonArray);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                if(position == attacker_pos) {
+                    sendBlinkRequestToServer(wrong_pattern_server[0],wrong_pattern_server[1],wrong_pattern_server[2]);
+                    makePhoneBlink(blinker,correct_pattern_phone[0],correct_pattern_phone[1],correct_pattern_phone[2]);
+                    Toast.makeText(getApplicationContext(), "asking " +selectedValue + " to blink " , Toast.LENGTH_SHORT).show();
+                }
+                else if(position == real_wifi_pos) {
+                    sendBlinkRequestToServer(correct_pattern_server[0],correct_pattern_server[1],correct_pattern_server[2]);
+                    makePhoneBlink(blinker,correct_pattern_phone[0],correct_pattern_phone[1],correct_pattern_phone[2]);
+                    Toast.makeText(getApplicationContext(), "asking " +selectedValue + " to blink " , Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), selectedValue + " doesn't support blinking" , Toast.LENGTH_SHORT).show();
                 }
 
-                mSocket.emit("news",colorsObj);
-                //blinker.setBackgroundColor(Color.parseColor("#00FF00"));
 
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        blinker.setBackgroundColor(Color.parseColor("#FF0000"));
-                        blinker.setVisibility(View.VISIBLE);
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                blinker.setBackgroundColor(Color.parseColor("#00FF00"));
-                                handler.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        blinker.setBackgroundColor(Color.parseColor("#0000FF"));
-                                        handler.postDelayed(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                blinker.setVisibility(View.INVISIBLE);
-                                            }
-                                        }, 1000);
-                                    }
-                                }, 1000);
-                            }
-                        }, 1000);
-                    }
-                }, 1000);
+
+
 
                 return true;
+
+//                JSONObject jsonObject1 = new JSONObject();
+//                JSONObject jsonObject2 = new JSONObject();
+//                JSONObject jsonObject3 = new JSONObject();
+//                JSONArray jsonArray = new JSONArray();
+//                JSONObject colorsObj = new JSONObject();
+//                try {
+//                    jsonObject1 = new JSONObject();
+//                    jsonObject1.put("color","red");
+//                    jsonObject2 = new JSONObject();
+//                    jsonObject2.put("color","green");
+//                    jsonObject3 = new JSONObject();
+//                    jsonObject3.put("color","blue");
+//
+//                    jsonArray.put(jsonObject1);
+//                    jsonArray.put(jsonObject2);
+//                    jsonArray.put(jsonObject3);
+//
+//                    colorsObj.put("colors", jsonArray);
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                mSocket.emit("news",colorsObj);
+                //blinker.setBackgroundColor(Color.parseColor("#00FF00"));
+
+//                final Handler handler = new Handler();
+//                handler.postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        blinker.setBackgroundColor(Color.parseColor("#FF0000"));
+//                        blinker.setVisibility(View.VISIBLE);
+//                        handler.postDelayed(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                blinker.setBackgroundColor(Color.parseColor("#00FF00"));
+//                                handler.postDelayed(new Runnable() {
+//                                    @Override
+//                                    public void run() {
+//                                        blinker.setBackgroundColor(Color.parseColor("#0000FF"));
+//                                        handler.postDelayed(new Runnable() {
+//                                            @Override
+//                                            public void run() {
+//                                                blinker.setVisibility(View.INVISIBLE);
+//                                            }
+//                                        }, 1000);
+//                                    }
+//                                }, 1000);
+//                            }
+//                        }, 1000);
+//                    }
+//                }, 1000);
+//
+//                return true;
             }
 
         });
